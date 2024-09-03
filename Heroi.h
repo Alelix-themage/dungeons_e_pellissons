9cf203d496c4e1023b131b2a7bc42f8c9c49e708
Felipe Ferreira <felipe.fr.almeida@gmail.com>
2024-09-03 15:59:30 -0300

    Nome Mateus

--- Heroi.h
+++ Heroi.h
@@ -1,3 +1,8 @@
+// INTEGRANTES:
+// Alessandro Luis Pinheiro da Rocha Junior - 223087
+// Felipe Ferreira Rangel de Almeida - 2208490
+// Mateus Tiraboschi de Castro - 2200040
+
 #include <string>
 #include <iostream>
 using namespace std;
