ef28c4869b22b475dbf18460359a3b3eaeb841e9
Alelix-themage <alessandrojunior864@gmail.com>
2024-09-03 12:42:20 -0300

    Inclua o arquivo heroi.h responsavel pelas funcionalidades do heroi

---
+++ Heroi.h
@@ -0,0 +1,12 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+//Classe responsável pelas funcionalidades e atributos do heroi
+class Heroi{
+    public:
+        Heroi();
+        ~Heroi();
+
+    private:
+};
