7dd573d087db630ad14000666a51ccd37916a57d
Felipe Ferreira <felipe.fr.almeida@gmail.com>
2024-09-04 07:12:04 -0300

    aspas remove

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 // INTEGRANTES:
 // Alessandro Luis Pinheiro da Rocha Junior - 223087
 // Felipe Ferreira Rangel de Almeida - 2208490
-// Mateus Tiraboschi de Castro - 2200040
+// Mateus Tiraboschi de Castro - 2200040"
 
 #include <iostream>
 #include <string>
