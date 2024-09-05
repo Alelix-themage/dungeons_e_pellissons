caba5aa19447e0f709d50f236283aa283e7a4eb2
Alelix-themage <alessandrojunior864@gmail.com>
2024-09-05 12:15:21 -0300

    Inclue as classes Heroi, mosntro, e a struct arma no arquivo Dungeons.h

---
+++ Dungeons.h
@@ -0,0 +1,45 @@
+// INTEGRANTES:
+// Alessandro Luis Pinheiro da Rocha Junior - 223087
+// Felipe Ferreira Rangel de Almeida - 2208490
+// Mateus Tiraboschi de Castro - 2200040
+
+#include <string>
+#include <iostream>
+#include <string>
+using namespace std;
+
+//Uma função Global que gera danos nos personagens
+void ReceberDano(int dano);
+
+
+//Classe responsável pelas funcionalidades e atributos do heroi
+class Heroi{
+    public:
+        Heroi();
+        ~Heroi();
+        void EscolherNome(string nome);
+
+    private:  
+        // nome e vida do Herói só pode ser acessada por métodos da classe herói
+        string nome; 
+        int vida;
+};
+
+//Classe responsável pelo comportamento dos objetos do tipo Monstro
+class Monstro {
+    public:
+        Monstro();
+        ~Monstro();
+        
+
+    private:
+        string nome;
+        int vida = 50;
+    
+};
+
+struct Arma {
+    //tipo de dado responsável pelas especificações da arma
+    int dano = 25;
+
+};
