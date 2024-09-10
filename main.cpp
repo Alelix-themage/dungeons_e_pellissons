0fb9a2bffaa2ec1d2fd69cb9b4f03a7681baa429
Felipe Ferreira <felipe.fr.almeida@gmail.com>
2024-09-10 09:56:59 -0300

    sortear sqm

--- main.cpp
+++ main.cpp
@@ -5,23 +5,65 @@
 
 #include <iostream>
 #include <string>
+#include <random>
 #include "Heroi.h"
 #include "Pocao.h"
 using namespace std;
 
+
+// VARIAVEIS
 const int larguradisplay = 52;
 const int tamanho_mapa = 15;
-const int num_niveis = 5;
-const string nomesniveis[num_niveis] = {"Estrada da Main", "Floresta Binaria", "Montanha Pilha de Neve", "Vulcao Recursivo", "Fortaleza dos Dados"}; // nomes de exemplo
+const int num_niveis = 2;
 const string RED = "\033[1;31m";
 const string RESET = "\033[0m";
 
+// ANUNCIAR EVETNO
+static const string frasesInimigo[5] = {
+    "Voce encontrou um inimigo feroz!",
+    "Um inimigo apareceu magicamente!",
+    "Cuidado! Um inimigo esta a espreita!",
+    "Um inimigo bloqueia seu caminho!",
+    "A sombra de um inimigo surge na sua frente!"
+};
+static const string frasesNada[5] = {
+    "O caminho esta vazio.",
+    "Você nao encontrou nada de interessante.",
+    "Nada a vista. Continue explorando.",
+    "Parece que nao ha nada aqui.",
+    "Tudo quieto. Pode tomar um ar."
+};
+static const string frasesTesouro[5] = {
+    "Você encontrou um bau de tesouro!",
+    "Parabens! Um tesouro esta a sua frente!",
+    "Tesouro descoberto! O que será que tem dentro?",
+    "Um brilho dourado revela um tesouro escondido!",
+    "Incrivel! Um tesouro misterioso!"
+};
+
+// NOMES NIVEIS
+const string nomesniveis[5] = {
+    "Estrada da Main", 
+    "Floresta Binaria", 
+    "Montanha Pilha de Neve", 
+    "Vulcao Recursivo", 
+    "Fortaleza dos Dados"
+};
+
+// GERADOR NUMEROS RADNOM
+random_device rd;
+mt19937 gen(rd());
+
+// FUNÇÕES
 void Logo();
 void Centralizar(int largura, string text);
-void display(int &nivelatual, int &sqmatual, Heroi player);
+void Display(int &nivelatual, int &sqmatual, Heroi player);
+void SortearEvento(short int &tipo);
+void AnunciarEvento(short int tipo);
+
 
 // g++ main.cpp Heroi.cpp Pocao.cpp -o jogo.exe  <=== COMPILAR
-// g++ -std=c++11 Heroi.cpp main.cpp -o jogo.exe
+// g++ -std=c++11 main.cpp Heroi.cpp Pocao.cpp -o jogo.exe
 
 // .\jogo.exe                <=== RODAR
 
@@ -37,32 +79,60 @@ int main(){
     Logo();
     Centralizar(larguradisplay,"PRESSIONE QUALQUER TECLA PARA INICIAR...");
     start = cin.get();
-    cout << "----------------------------------------------------\n" << RESET;
+    cout << RED << "----------------------------------------------------\n" << RESET;
     cout << RED << " - Nome do seu heroi -----> " << RESET;
     string n;
     getline(cin, n);
     player.DefinirNome(n);
-    cout << RED << "----------------------------------------------------\n" << RESET;
+    
 
     while(!fim){
-        //Displayy Caraiii
-        display(nivelatual, sqmatual, player);
-        Pocao p1;
+        //Display
+        Display(nivelatual, sqmatual, player);
+        
+        // Iniciar Round
+        short int tipoevento;
+        SortearEvento(tipoevento);
+        AnunciarEvento(tipoevento);
+        start = cin.get();
 
-        player.TomarPocao(p1.RetornarCura());
+        // Switch entre batalha, vazio e elemento
+        switch (tipoevento)
+        {
+        case 1:
+            // BATALHA
 
+            break;
+        case 2:
+            // VAZIO
+
+            break;
+        case 3:
+            // ELEMENTO
+
+            break;
+        default:
+            break;
+        }
+
+        
         // Finalizar "Round"
+        if (nivelatual == num_niveis && sqmatual == tamanho_mapa){
+            fim = true;
+        }
         if (sqmatual == tamanho_mapa){
             nivelatual++;
             sqmatual = 1;
         } else {
             sqmatual++;
         }
-        if (nivelatual == num_niveis && sqmatual == tamanho_mapa){
-            fim = true;
-        }
     }
-    cout << "FIM";
+
+    // FIM DO JOGO
+    cout << "\n\n" << RED;
+    Centralizar(larguradisplay, "FIM DO JOGO");
+    Logo();
+    
 
     return 0;
 }
@@ -82,13 +152,13 @@ void Logo(){
     cout << "                             {}  /  \\_/\\=/\\_/  \\\n";
     cout << "----------------------------------------------------\n";
     Centralizar(larguradisplay, "-= DUNGEONS & PELLISSONS =-");
-    cout << "--------------------------------------------------"  << endl;;
 }
 
-void display(int &nivelatual, int &sqmatual, Heroi player){
+void Display(int &nivelatual, int &sqmatual, Heroi player){
+    cout << RED << "----------------------------------------------------\n" << RESET;
     cout << RED << "NIVEL: " << RESET << nivelatual << RED << " | " << nomesniveis[nivelatual-1] << " | PROGRESSO: " << RESET << sqmatual << "/" << tamanho_mapa << endl;
     cout << RED << "----------------------------------------------------\n" << RESET;
-    cout << RED << "HEROI: " << RESET << player.Nome() << RED << " | HP: " << player.RetornarHP() << endl;
+    cout << RED << "HEROI: " << RESET << player.Nome() << RED << " | HP: " << RESET << player.RetornarHP() << endl;
     cout << RED << "----------------------------------------------------\n\n" << RESET;
 }
 
@@ -100,4 +170,29 @@ void Centralizar(int largura, string texto){
     }
 
     cout << texto << endl;
+}
+
+void SortearEvento(short int &tipo){
+    uniform_int_distribution<> distr(1, 3);
+    tipo = distr(gen);
+}
+
+void AnunciarEvento(short int evento){
+    uniform_int_distribution<> distr(0, 4);
+    int random = distr(gen);
+
+    switch (evento)
+        {
+        case 1:
+            cout << RED << frasesInimigo[random] << endl;;
+            break;
+        case 2:
+            cout << RED << frasesNada[random] << endl;;
+            break;
+        case 3:
+            cout << RED << frasesTesouro[random] << endl;;
+            break;
+        default:
+            break;
+        }
 }
